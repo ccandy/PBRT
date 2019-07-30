5fca59add49bcceee4f93e2b9f911d711ba28ad6
ccandy <ccunandy2001@gmail.com>
2019-07-30 15:01:18 +0800

    add vectors

---
+++ PBRSystem/Assets/Script/Core/Vectors/PBRVector2.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+public class PBRVector2
+{
+    // Start is called before the first frame update
+
+    #region Variable
+    private float _x;
+    private float _y;
+
+    public float X
+    {
+        set
+        {
+            _x = value;
+        }
+        get
+        {
+            return _x;
+        }
+    }
+
+    public float Y
+    {
+        set
+        {
+            _y = value;
+        }
+        get
+        {
+            return _y;
+        }
+    }
+    #endregion
+    #region Constructor
+    public PBRVector2()
+    {
+        _x = _y = 0;
+    }
+
+    public PBRVector2(float x, float y)
+    {
+        _x = x;
+        _y = y;
+    }
+
+    public PBRVector2(PBRVector2 v)
+    {
+        _x = v.X;
+        _y = v.Y;
+    }
+    #endregion
+
+
+    #region Operator
+    public static PBRVector2 operator +(PBRVector2 a, PBRVector2 b)
+    {
+        return new PBRVector2(a.X + b.X, a.Y + b.Y);
+    }
+
+    public static PBRVector2 operator -(PBRVector2 a, PBRVector2 b)
+    {
+        return new PBRVector2(a.X - b.X, a.Y - b.Y);
+    }
+
+    public static PBRVector2 operator *(PBRVector2 a, PBRVector2 b)
+    {
+        return new PBRVector2(a.X * b.X, a.Y * b.Y);
+    }
+
+    public static bool operator==(PBRVector2 a, PBRVector2 b)
+    {
+        return (a.X == b.X) && (a.Y == b.Y);
+    }
+
+    public static bool operator !=(PBRVector2 a, PBRVector2 b)
+    {
+        return (a.X != b.X) && (a.Y != b.Y);
+    }
+    
+    public static PBRVector2 operator /(PBRVector2 a, PBRVector2 b)
+    {
+        return new PBRVector2(a.X / b.X,a.Y / b.Y);
+    }
+
+
+    override
+    public bool Equals(object obj)
+    {
+        if(obj is PBRVector2)
+        {
+            PBRVector2 v = (PBRVector2)obj;
+            return (this._x == v.X && this._y == v.Y);
+        }
+        return false;
+    }
+    override
+    public int GetHashCode()
+    {
+        return _x.GetHashCode() + _y.GetHashCode();
+    }
+
+
+    #endregion
+
+
+
+}
---
+++ PBRSystem/Assets/Script/Core/Vectors/PBRVector3.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+public class PBRVector3
+{
+    // Start is called before the first frame update
+
+    #region Variable
+    private float _x;
+    private float _y;
+    private float _z;
+
+    public float X
+    {
+        set
+        {
+            _x = value;
+        }
+        get
+        {
+            return _x;
+        }
+    }
+
+    public float Y
+    {
+        set
+        {
+            _y = value;
+        }
+        get
+        {
+            return _y;
+        }
+    }
+    public float Z
+    {
+        set
+        {
+            _z = value;
+        }
+        get
+        {
+            return _z;
+        }
+    }
+
+
+    #endregion
+    #region Constructor
+    public PBRVector3()
+    {
+        _x = _y = _z = 0;
+    }
+
+    public PBRVector3(float x, float y, float z)
+    {
+        _x = x;
+        _y = y;
+        _z = z;
+    }
+
+    public PBRVector3(PBRVector3 v)
+    {
+        _x = v.X;
+        _y = v.Y;
+        _z = v.Z;
+    }
+    #endregion
+
+
+    #region Operator
+    public static PBRVector3 operator +(PBRVector3 a, PBRVector3 b)
+    {
+        return new PBRVector3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+    }
+
+    public static PBRVector3 operator -(PBRVector3 a, PBRVector3 b)
+    {
+        return new PBRVector3(a.X - b.X, a.Y - b.Y,a.Z - b.Z);
+    }
+
+    public static PBRVector3 operator *(PBRVector3 a, PBRVector3 b)
+    {
+        return new PBRVector3(a.X * b.X, a.Y * b.Y, a.Z * b.Z);
+    }
+
+    public static bool operator ==(PBRVector3 a, PBRVector3 b)
+    {
+        return (a.X == b.X) && (a.Y == b.Y) && (a.Z == b.Z);
+    }
+
+    public static bool operator !=(PBRVector3 a, PBRVector3 b)
+    {
+        return (a.X != b.X) && (a.Y != b.Y) && (a.Z != b.Z);
+    }
+
+    public static PBRVector3 operator /(PBRVector3 a, PBRVector3 b)
+    {
+        return new PBRVector3(a.X / b.X, a.Y / b.Y);
+    }
+
+
+    override
+    public bool Equals(object obj)
+    {
+        if (obj is PBRVector3)
+        {
+            PBRVector3 v = (PBRVector3)obj;
+            return (this._x == v.X && this._y == v.Y);
+        }
+        return false;
+    }
+    override
+    public int GetHashCode()
+    {
+        return _x.GetHashCode() + _y.GetHashCode();
+    }
+
+
+    #endregion
+
+
+
+}
