240b501dc320f8b1d407284fb628c8c3f1329e9a
ccandy <ccunandy2001@gmail.com>
2019-08-07 18:11:58 +0800

    add matrix4x4 add PBRTransform

---
+++ PBRSystem/Assets/Script/Core/CoordSystem/PBRTransform.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+public class PBRTransform
+{
+    private matrix4X4
+    public PBRTransform() { }
+
+
+}
---
+++ PBRSystem/Assets/Script/Core/Math/Matrix4x4.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Matrix4x4
+{
+    private float[,] _m = new float[4,4];
+    public float[,] M
+    {
+        get
+        {
+            return _m;
+        }
+
+        set
+        {
+            _m = value;
+        }
+
+    }
+
+    public Matrix4x4()
+    {
+        _m[0, 0] = _m[1, 1] = _m[2, 2] = _m[3, 3] = 1.0f;
+
+    }
+
+
+
+
+}
